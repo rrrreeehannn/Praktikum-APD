e9c87793ac6da9e9f2aa1b96e80664a4544cb7e1
rrrreeehannn <muhammadrayhan1011@gmail.com>
2024-09-20 15:37:51 +0800

    File coding kelas

---
+++ Kelas B2 24/Kelas/pertemuan  3/Pengumpulan Code Pertemuan 3.py
@@ -0,0 +1,101 @@
+#cuaca = "cerah"
+
+#if cuaca == "cerah":
+ #    print("kamu pergi keluar rumah")
+     
+#cuaca = "cerah"
+#if true:
+#   print("kamu pergi keluar rumah")
+
+# cuaca = "cerah
+
+# if cuaca == "cerah
+# # print("kamu pergi keluar rumah: ")
+
+# cuaca = input("masukkan cuaca hari ini: ")
+# if cuaca == "cerah":
+#     print ("kamu keluar rumah")
+
+# cuaca = input("masukkan cuaca hari ini: ")
+# if cuaca == "cerah":
+#     print("kamu keluar rumah")
+# else:
+#     print("kamu tetap di rumah")
+
+#cuaca = input("masukkan cuaca hari ini: ")
+#if cuaca == "cerah":
+#   print("kamu keluar rumah")
+#elif cuaca == "mendung":
+#    print("baca komik") 
+#else:
+#    print("kamu tidur")  
+    
+#opsi = input("""pilih menu:
+#1. kondisi 1
+#2. kondisi 2
+#3. kondisi 3""")
+#if opsi == "1":
+#     print("kondisi 1")
+#elif opsi == "2":
+#     print("kondisi 2")
+#elif opsi == "3":
+#    print("kondisi 3") 
+#else:
+#    print("input tidak valid")
+    
+    
+    
+    
+#umur = int(input("masukkan umur: ")
+# if umur <0:
+#     print("input tidak valid")          
+#elif (umur <= 10) :
+#    print("anak-anak")
+#elif (umur <= 18):
+#    print("remaja")
+#elif (umur <= 60):
+#    print("dewasa"):
+        
+# angka = int(input("maukkan angka: ")) 
+# result = "genap" if angka % 2 == 0 else "ganjil"  
+
+# print(angka,"adalah angka", result)
+
+a = 10
+b = 20
+print("a lebih besar dari b") if a > b else print("a lebih kecil dari b")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print("""pilih menu
+# 1. kondisi 1
+# 2. kondisi 2
+# 3. kondisi 3""")
+opsi = input("pilih menu: ")
+match opsi:
+     case "1":
+         print("kondisi 1")
+     case "2":
+         print("kondisi 2")  
+     case "3":
+         print("kondisi 3") 
+     case _:
+         print("input tidak valid")   
