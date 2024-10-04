13177436e13bb17027b43cd16dcdc4ff51949bcd
rrrreeehannn <muhammadrayhan1011@gmail.com>
2024-10-04 15:36:24 +0800

    Add files via upload

---
+++ Kelas B2 24/pertemuan 5/pertemuan5.py
@@ -0,0 +1,79 @@
+list1 = ["a",2,[1,2,3]]
+
+print (type[list-1][2])
+
+
+
+
+
+
+tas = ["buku", 32, True, 3.14, ["IF, 24"]]
+
+for i in tas:
+    print (i)
+    
+        
+for i in rane(len(tas)):
+    print(tas[i])
+    
+    
+matkul = ["kalkulus", "fisdas", "pti"]
+print (matkul)
+
+matkul.append("sastra mesin")
+matkul.insert(1,"Tes")
+
+matkul[1] = " Fisika Quantum"
+print(matkul)
+
+del matkul [1]
+print(matkul)
+
+buang = matkul.pop(1)
+print(matkul)
+
+print(f"variabel buang ={buang} ")
+
+prodi =["Informatika", "Sistem Informasi", "Teknik Arsitektur", "Teknik Lingkungan", 
+"Teknik Pertambangan", "Teknik Elektro"
+"Teknik Industri", "Teknik Sipil"
+"Teknik Geologi", "Teknik KImia"]
+
+
+print(prodi[::2],"\n", prodi)
+
+pyrochar = ["hi tao", "klee", "xiangling"]
+hydo
+
+
+print(pyrochar, "\n")
+print(hydrochar, "\n")
+print( party)
+
+print(pyrochar*3)
+
+barang = [["sepatu","tas","baju" ],
+          ["pulpen","pensil","laptop"]]
+print(barang [1][1])
+print(barang)
+for i in barang:
+    for j in i:
+        print(j)
+        
+nama = ("aldi", "dsffs", "arishyi")
+
+print(nama)
+nama.append("rava")
+print(nama)
+
+
+
+
+
+
+
+
+mahasiswa = (69, "Informatika", "2209106044", "Aldy septian ")
+#lalu kita unpacking
+absen, prodi, = mahasiswa
+#maka ketiga variabel akan berisi data diri tuple mahasiswa 
